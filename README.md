97a29ede2fad668e837fd7ac0196f14404bb8aef
singhgauravv <gauravs49804@gmail.com>
2024-06-12 23:03:27 +0530

    Implement Image recognition.

--- README.md
+++ README.md
@@ -6,6 +6,28 @@ Given the pixel intensity values in an image, identify whether the character is 
 
 ## Accuracy Achieved:
 
+## Data source: MNIST database (Modified National Institute of Standards and Technology database)
+
+## How to encode features?
+
+In the context of image recognition tasks using the MNIST dataset with TensorFlow.js and JavaScript, encoding features involves representing each image's pixel values in a format suitable for machine learning algorithms.
+
+#### Encoding Pixel Values:
+
+Each image in the MNIST dataset consists of a 28x28 grid of pixels, totaling 784 pixels per image. To encode these pixels, we flatten the 28x28 grid into a single array containing 784 elements. This array represents the grayscale values of each pixel in the image.
+
+#### Array Organization:
+
+The flattened pixel array for each image is then nested within an outer array. This outer array serves as a container for all the image data in the dataset.
+
+## How to encode label values?
+
+In our case, the total number of possible label values are going to be 10, i.e 0 to 9. To encore a optimal label values encoding,
+we will create an array that will contain 1 at the index equal to the label value and otherwise 0.
+
+For example, to represent label 5, the encoding will be [0,0,0,0,0,1,0,0,0,0].
+To represent label 0, the encoding will be [1,0,0,0,0,0,0,0,0,0], and so on.
+
 ## Toolkit:
 
 1. JavaScript
---
+++ logistic-regression.js
@@ -0,0 +1,140 @@
+const tf = require("@tensorflow/tfjs");
+const _ = require("lodash");
+
+class LogisticRegression {
+  constructor(features, labels, options) {
+    this.features = this.processFeatures(features);
+    this.labels = tf.tensor(labels);
+    /** cost function, scientific referring to cross entropy */
+    this.costHistory = [];
+
+    this.options = Object.assign(
+      // Default values.
+      {
+        learningRate: 0.1,
+        iterations: 1000,
+        batchSize: 10,
+        decisionBoundary: 0.5,
+      },
+      options
+    );
+
+    /** The following code initializes the value of weights. */
+    this.weights = tf.zeros([this.features.shape[1], this.labels.shape[1]]);
+  }
+
+  gradientDescent(features, labels) {
+    /**
+     * Calculate the gradients of the Mean Squared Error (MSE) with respect to weights (including bias).
+     * Formula: (transpose of Features * ((Features * Weights) - Labels)) / n * 2
+     */
+    const currentGuesses = features.matMul(this.weights).softmax();
+    const differences = currentGuesses.sub(labels);
+
+    const slopes = features
+      .transpose() // Transpose of the feature matrix
+      .matMul(differences) // Dot product with prediction errors
+      .div(features.shape[0]); // Average over the number of data points
+    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+  }
+
+  train() {
+    const batchQuantity = Math.floor(
+      this.features.shape[0] / this.options.batchSize
+    );
+
+    for (let i = 0; i < this.options.iterations; i++) {
+      /** Implement batch gradient descent using tensorflowjs. */
+      for (let j = 0; j < batchQuantity; j++) {
+        const startIndex = j * this.options.batchSize;
+        const { batchSize } = this.options;
+        const featureSlice = this.features.slice(
+          [startIndex, 0],
+          [batchSize, -1]
+        );
+
+        const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
+        this.gradientDescent(featureSlice, labelSlice);
+      }
+
+      this.recordCost();
+      this.updateLearningRate();
+    }
+  }
+
+  predict(observations) {
+    /** Standardize observations
+     */
+    return this.processFeatures(observations)
+      .matMul(this.weights)
+      .softmax()
+      .argMax(1);
+  }
+
+  test(testFeatures, testLabels) {
+    const predictions = this.predict(testFeatures);
+    testLabels = tf.tensor(testLabels).argMax(1);
+
+    const incorrect = predictions.notEqual(testLabels).sum().arraySync();
+
+    /** % correct:  (total predictions - Incorrect Predictions)/ total predictions */
+    return (predictions.shape[0] - incorrect) / predictions.shape[0];
+  }
+
+  processFeatures(features) {
+    features = tf.tensor(features);
+
+    if (this.mean && this.variance) {
+      features = features.sub(this.mean).div(this.variance.pow(0.5));
+    } else {
+      features = this.standardize(features);
+    }
+
+    features = tf.ones([features.shape[0], 1]).concat(features, 1);
+
+    return features;
+  }
+
+  standardize(features) {
+    const { mean, variance } = tf.moments(features, 0);
+
+    this.mean = mean;
+    this.variance = variance;
+
+    return features.sub(mean).div(variance.pow(0.5));
+  }
+
+  recordCost() {
+    /** Calculate cross entropy. */
+    const guesses = this.features.matMul(this.weights).sigmoid();
+    const termOne = this.labels.transpose().matMul(guesses.log());
+
+    const termTwo = this.labels
+      .mul(-1)
+      .add(1)
+      .transpose()
+      .matMul(guesses.mul(-1).add(1).log());
+
+    const cost = termOne
+      .add(termTwo)
+      .div(this.features.shape[0])
+      .mul(-1)
+      .arraySync();
+
+    this.costHistory.unshift(cost);
+  }
+
+  updateLearningRate() {
+    if (this.costHistory.length < 2) {
+      return;
+    }
+
+    if (this.costHistory[0] > this.costHistory[1]) {
+      this.options.learningRate /= 2;
+    } else {
+      this.options.learningRate *= 1.05;
+    }
+  }
+}
+
+module.exports = LogisticRegression;
